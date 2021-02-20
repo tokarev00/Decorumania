6ad31f621fccec3db069ae2849b4a1be4d077b2f
tokarev00 <65186416+tokarev00@users.noreply.github.com>
2021-02-20 15:53:20 +0200

    finished policy page

--- js/main.js
+++ js/main.js
@@ -1,12 +1,35 @@
-new CircleType(document.querySelector('.intro-circle__title'));
-new CircleType(document.querySelector('.intro-circle__slogan'));
-let whatWeDoCircleTitle = new CircleType(document.querySelector('.what-we-do-circle-title'));
-
-whatWeDoCircleTitle.radius(180);
 const burgerLogo = document.querySelector('.burger-logo');
 const menuNavList = document.querySelector('.nav ul');
 const menuNavItems = document.querySelectorAll('.nav li');
 const body = document.body;
+
+const homeItems ={
+    intro: {
+            circleTitle: document.querySelector('.intro-circle__title'),
+            circleSlogan: document.querySelector('.intro-circle__slogan'),
+    },
+    whatWeDo: {
+            circleTitle: document.querySelector('.what-we-do-circle-title'),
+    },
+}
+// const portfolioItems = {
+//     introPortfolio: {
+//         circleTitle: document.querySelector('.portfolio-intro-circle__title'),
+//     }
+// }
+
+
+
+if(homeItems.intro.circleTitle && homeItems.intro.circleSlogan && homeItems.whatWeDo.circleTitle){
+    new CircleType(homeItems.intro.circleTitle);
+    new CircleType(homeItems.intro.circleSlogan);
+    new CircleType(homeItems.whatWeDo.circleTitle);
+}
+
+// if(portfolioItems.introPortfolio.circleTitle){
+//     new CircleType(portfolioItems.introPortfolio.circleTitle);
+// }
+
 //menu
 burgerLogo.addEventListener('click', ()=>{
     burgerLogo.classList.toggle('active');
