471dad70e3eeb7f09ae97c29f91b51e5fe258128
tokarev00 <65186416+tokarev00@users.noreply.github.com>
2021-03-07 17:19:15 +0200

    added animation on index-page

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const burgerLogo = document.querySelector('.burger-logo');
 const menuNavList = document.querySelector('.nav ul');
 const menuNavItems = document.querySelectorAll('.nav li');
 const body = document.body;
+const animItems = document.querySelectorAll('._anim-item');
 
 const homeItems ={
     intro: {
@@ -34,3 +35,38 @@ menuNavItems.forEach((elem)=>{
         body.classList.remove('overflow-hidden');
     })
 })
+
+
+if(animItems.length > 0){
+    window.addEventListener('scroll', animOnScroll);
+    function animOnScroll(){
+        for(let index = 0 ; index < animItems.length; index++){
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+            if( animItemHeight > window.innerHeight){
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)){
+                animItem.classList.add('active');
+            }else{
+                if(!animItem.classList.contains('_anim-no-hide')){
+                    animItem.classList.remove('active');
+                }
+            }
+        }
+    }
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+    }
+    setTimeout(()=>{
+        animOnScroll();
+    }, 300)
+}
